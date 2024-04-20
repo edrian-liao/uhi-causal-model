0e3a77123e599cd99d75a6622c01182e8e1d2437
edrian-liao <edrianpaul.liao@duke.edu>
2024-04-20 13:44:55 -0400

    finished bootstrapping

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,14 +5,17 @@
 #SBATCH --output=./bootstrap.out
 #SBATCH --error=./bootstrap.err
 
+source ~/.bashrc
+conda activate research
+
 python ../bootstrap_coefficients.py \
     --data_dir ../data/boston/ \
     --output_dir ../results/bootstrap/ \
     --num_blocks 50 \
     --l2_alpha 5 \
-    --ndvi_ls 15 \
-    --albedo_ls 10 \
-    --gp_noise 1e-1 \
+    --ndvi_ls 29 \
+    --albedo_ls 17 \
+    --gp_noise 1e-4 \
     --gp_constant_1 0.5 \
     --gp_constant_2 3e-7 \
     --gp_length_scale 115 \
