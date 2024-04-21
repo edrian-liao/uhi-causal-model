8764a8a5dbb551d404fa3fa32a12f346819114cc
Edrian Liao <edrianpaul.liao@duke.edu>
2024-04-21 11:40:09 -0400

    try f1

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -12,10 +12,10 @@ python ../bootstrap_coefficients.py \
     --data_dir ../data/boston/ \
     --output_dir ../results/bootstrap/ \
     --num_blocks 50 \
-    --l2_alpha 0.1 \
-    --ndvi_ls 17 \
-    --albedo_ls 4 \
-    --gp_noise 1e-2 \
+    --l2_alpha 1 \
+    --ndvi_ls 26 \
+    --albedo_ls 8 \
+    --gp_noise 1e-4 \
     --gp_constant_1 0.5 \
     --gp_constant_2 3e-7 \
     --gp_length_scale 115 \
